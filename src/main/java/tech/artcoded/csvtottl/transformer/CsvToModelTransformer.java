9c37fd97bc170b3836c632aec0fa0187dd01c491
Nordine Bittich <nordine.bittich@ext.europa.ec.eu>
2021-01-07 15:35:55 +0100

    directly on nightmare

--- src/main/java/tech/artcoded/csvtottl/transformer/CsvToModelTransformer.java
+++ src/main/java/tech/artcoded/csvtottl/transformer/CsvToModelTransformer.java
@@ -7,6 +7,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
@@ -19,6 +20,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
+import static tech.artcoded.csvtottl.transformer.DataTransformer.addResource;
 
 @Service
 @Slf4j
@@ -62,15 +64,20 @@ public class CsvToModelTransformer implements CommandLineRunner {
         List<Map<String, String>> csvDdenominations = CSVReaderUtils.readMap(denominationCsv.getInputStream());
 
         Map<String, List<Map<String, String>>> groupedResources = csvDdenominations.stream().
-                collect(Collectors.groupingBy(map -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "denomination", map.get("EntityNumber").replaceAll("\\.", ""))));
+                collect(Collectors.groupingBy(map -> map.get("EntityNumber").replaceAll("\\.", "")));
 
         groupedResources.forEach((key, value) -> {
             Model model = ModelFactory.createDefaultModel();
 
-            org.apache.jena.rdf.model.Resource resource = model.createResource(key);
+            org.apache.jena.rdf.model.Resource resource = model.createResource("%s/%s/%s".formatted(NAMESPACE_PREFIX, "denomination", key));
+
+            var organizationUri = "%s/%s/%s".formatted(NAMESPACE_PREFIX, "company", key);
+            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasOrganization"), organizationUri, List.of(ORG_TYPE), false);
+
+            org.apache.jena.rdf.model.Resource labelResource = model.createResource(NAMESPACE_PREFIX + "/label");
+
             value.stream().map(v -> {
-                var organizationUri = "%s/%s/%s".formatted(NAMESPACE_PREFIX, "company", v.get("EntityNumber").replaceAll("\\.", ""));
-                DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasOrganization"), organizationUri, List.of(ORG_TYPE), false);
+
                 String lang = switch (v.get("Language")) {
                     case "2" -> "nl";
                     case "3" -> "de";
@@ -81,8 +88,9 @@ public class CsvToModelTransformer implements CommandLineRunner {
             })
                     .filter(entry -> StringUtils.isNotEmpty(entry.getKey()) && StringUtils.isNotEmpty(entry.getValue()))
                     .forEach(entry -> {
-                        DataTransformer.addLangLiteral(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/companyName"), entry.getKey(), entry.getValue(), false);
+                        DataTransformer.addLangLiteral(labelResource, RDFS.label, entry.getKey(), entry.getValue(), false);
                     });
+            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasLabel"), labelResource.getURI(), List.of(RDFS.label), false);
             model.add(resource, RDF.type, DENOMINATION_TYPE);
             VirtuosoUploadUtils.upload(model,defaultGraphUri,host, username, password);
 
@@ -93,25 +101,26 @@ public class CsvToModelTransformer implements CommandLineRunner {
     private void transformContacts() {
         log.info("create contact turtle file...");
 
-
         List<Map<String, String>> csvContacts = CSVReaderUtils.readMap(contactCsv.getInputStream());
 
         Map<String, List<Map<String, String>>> groupedResources = csvContacts.stream().
-                collect(Collectors.groupingBy(map -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "contact", map.get("EntityNumber").replaceAll("\\.", ""))));
+                collect(Collectors.groupingBy(map -> map.get("EntityNumber").replaceAll("\\.", "")));
 
         groupedResources.forEach((key, value) -> {
             Model model = ModelFactory.createDefaultModel();
 
-            org.apache.jena.rdf.model.Resource resource = model.createResource(key);
-            value.forEach(v -> {
-                var organizationUri = "%s/%s/%s".formatted(NAMESPACE_PREFIX, "company", v.get("EntityNumber").replaceAll("\\.", ""));
-                DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasOrganization"), organizationUri, List.of(ORG_TYPE), false);
+            org.apache.jena.rdf.model.Resource resource = model.createResource("%s/%s/%s".formatted(NAMESPACE_PREFIX, "contact", key));
+            var organizationUri = "%s/%s/%s".formatted(NAMESPACE_PREFIX, "company", key);
 
+            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasOrganization"), organizationUri, List.of(ORG_TYPE), false);
+
+            value.forEach(v -> {
                 ofNullable(v.get("ContactType")).filter(StringUtils::isNotEmpty).map(ct -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "ContactType", ct))
                         .ifPresentOrElse(uri -> {
-                            DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasContactType"), uri, List.of(CODE_TYPE), false);
+                            org.apache.jena.rdf.model.Resource contactTypeResource = model.createResource(uri);
+                            DataTransformer.addLiteral(contactTypeResource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/value"), v.get("Value"), false);
+                            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasContactType"), contactTypeResource.getURI(), List.of(CODE_TYPE), false);
                         }, () -> log.trace("'contact type' not found"));
-                DataTransformer.addLiteral(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/value"), v.get("Value"), false);
 
             });
 
@@ -140,21 +149,24 @@ public class CsvToModelTransformer implements CommandLineRunner {
             value.forEach(line -> {
                 ofNullable(line.get("Status")).filter(StringUtils::isNotEmpty).map(statusCode -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "Status", statusCode))
                         .ifPresentOrElse(statusUri -> {
-                            DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasStatus"), statusUri, List.of(CODE_TYPE), false);
+                            org.apache.jena.rdf.model.Resource statusResource = model.createResource(statusUri);
+                            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasStatus"), statusResource.getURI(), List.of(CODE_TYPE), false);
                         }, () -> log.trace("'status' not found"));
 
                 ofNullable(line.get("JuridicalSituation"))
                         .filter(StringUtils::isNotEmpty)
                         .map(jsCode -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "JuridicalSituation", jsCode))
                         .ifPresentOrElse(juridicalSituationUri -> {
-                            DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasJuridicalSituation"), juridicalSituationUri, List.of(CODE_TYPE), false);
+                            org.apache.jena.rdf.model.Resource jsResource = model.createResource(juridicalSituationUri);
+                            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasJuridicalSituation"), jsResource.getURI(), List.of(CODE_TYPE), false);
                         }, () -> log.trace("'juridicalSituationUri' not found"));
 
                 ofNullable(line.get("TypeOfEnterprise"))
                         .filter(StringUtils::isNotEmpty)
                         .map(typeOfEntrepriseCode -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "TypeOfEnterprise", typeOfEntrepriseCode))
                         .ifPresentOrElse(typeOfEntrepriseUri -> {
-                            DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasTypeOfCompany"), typeOfEntrepriseUri, List.of(CODE_TYPE), false);
+                            org.apache.jena.rdf.model.Resource toeResource = model.createResource(typeOfEntrepriseUri);
+                            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasTypeOfCompany"), toeResource.getURI(), List.of(CODE_TYPE), false);
                         }, () -> log.trace("'typeOfEntrepriseUri' not found"));
 
 
@@ -163,7 +175,8 @@ public class CsvToModelTransformer implements CommandLineRunner {
                         .map(jfCode -> "%s/%s/%s".formatted(NAMESPACE_PREFIX, "JuridicalForm", jfCode))
 
                         .ifPresentOrElse(juridicalFormUri -> {
-                            DataTransformer.addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasJuridicalForm"), juridicalFormUri, List.of(CODE_TYPE), false);
+                            org.apache.jena.rdf.model.Resource jfResource = model.createResource(juridicalFormUri);
+                            addResource(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasJuridicalForm"), jfResource.getURI(), List.of(CODE_TYPE), false);
                         }, () -> log.trace("'juridicalFormUri' not found"));
 
                 DataTransformer.addLiteral(resource, ResourceFactory.createProperty(NAMESPACE_PREFIX + "/hasStartDate"), line.get("StartDate"), false);
